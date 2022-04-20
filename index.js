95b125d1089f43535c47ad87b10a19bea5469518
hamza elglaoui <chyoin188@gmail.com>
2022-04-20 22:56:48 +0000

    change facebbok account

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const { scrollPageToBottom } = require("puppeteer-autoscroll-down");
   const page = await browser.newPage();
   await page.goto("https://m.facebook.com");
   await page.waitForSelector("#login_form");
-  await page.type("#m_login_email", "panicuq.lujoqa@labworld.org");
+  await page.type("#m_login_email", "artan@rujbreath.com");
   await page.type("#m_login_password", "Puppeteer123");
   await page.click("#login_form button");
   await page.waitForNavigation();
@@ -15,12 +15,10 @@ const { scrollPageToBottom } = require("puppeteer-autoscroll-down");
   await page.waitForNavigation();
   const elements = await page.$$("div._50xr");
   await page.waitForSelector("i.img");
-
   for (let i = 0; i < elements.length; i++) {
     try {
       await elements[i].screenshot({ path: `${i}.png` });
-      await page.waitForTimeout(1500);
-      await scrollPageToBottom(page, { size: 500 });
+      await scrollPageToBottom(page);
     } catch (e) {
       console.log(
         `couldnt take screenshot of element with index: ${i}. cause: `,
